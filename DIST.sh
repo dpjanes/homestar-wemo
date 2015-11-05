a917ceb9fa2f05e7fd18ec0807fea0e92127719a
David Janes <davidjanes@davidjanes.com>
2015-11-05 11:04:14 -0500

    make sure JSON is pushed with the release

--- DIST.sh
+++ DIST.sh
@@ -31,7 +31,7 @@ echo "=================="
 
     tar cf - \
         README.md LICENSE homestar.json package.json \
-        models/*js \
+        models/*js models/*.json \
         WeMoBridge.js index.js \
         |
     ( cd "${NPM_DST}" && tar xvf - )
