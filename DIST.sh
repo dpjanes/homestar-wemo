3ea00725bcd9ce0f266957ec32b3b0c13da9633a
David Janes <davidjanes@davidjanes.com>
2015-12-26 12:38:49 -0330

    auto commit/push package.json

--- DIST.sh
+++ DIST.sh
@@ -34,12 +34,9 @@ echo "=================="
         models/*js models/*.json \
         WeMoBridge.js index.js \
         |
-    ( cd "${NPM_DST}" && tar xvf - )
-
-    ## cp dist/*.* "${NPM_DST}" || exit 1
-
-    cd "${NPM_DST}" || exit 1
-    npm publish
+    ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
+    git commit -m "new release" package.json || exit 1
+    git push || exit 1
 
     echo "end"
 )
